c68b26f87ceea88dfd20b87ab79d7e962836bfa0
Stuart Kaija <stuartkaija@gmail.com>
2022-05-09 12:46:24 -0700

    form component adding user input to prompt state

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Form.scss';
 
 export default function Form() {
+
+    const [prompt, setPrompt] = useState('');
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const userInput = event.target.prompt.value;
+        setPrompt(userInput);
+        event.target.reset();
+    }
+
+    //useEffect to make call to API
+    useEffect(() => {
+        axios
+            .get()
+            .then()
+            .catch()
+    }, [prompt])
+
     return (
-        <form>
+        <form name='promptForm' id='promptForm' onSubmit={handleSubmit}>
             <h2>Submit a prompt to the AI overlords</h2>
-            <label htmlFor=""></label>
-            <textarea name="" id="" cols="30" rows="10" placeholder='Enter a prompt...'></textarea>
-            <button>Submit</button>
+            <label htmlFor="prompt">Enter prompt here</label>
+            <textarea name="prompt" id="prompt" cols="30" rows="10" placeholder='For example, tell me a story about a young elf from Venus.'></textarea>
+            <button type='submit' form='promptForm'>Submit</button>
         </form>
     )
 }
