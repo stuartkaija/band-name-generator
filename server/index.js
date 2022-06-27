8a4d62f0492ab2b7587e1f0d4299726dc43bd728
Stuart Kaija <stuartkaija@gmail.com>
2022-06-27 15:13:28 -0700

    updated cors options

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200
+}
 const { default: axios } = require('axios');
 
 // config
@@ -10,7 +15,7 @@ const port = process.env.PORT || 8081;
 // middleware
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // routes
 app.post("/", (req, res) => {
