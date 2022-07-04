ac6e8892d8e48e1bd8988619ed9fa75d2883e031
Stuart Kaija <stuartkaija@gmail.com>
2022-07-04 15:50:23 -0700

    updated about and footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <a href="https://www.linkedin.com/in/stuartkaija/" target='_blank' rel='noreferrer'>
             <img className='footer__icon--LI' src={LI} alt="LinkedIn" />
         </a>
-        <a href="https://github.com/stuartkaija/calculator" target='_blank' rel='noreferrer'>
+        <a href="https://github.com/stuartkaija/band-name-generator" target='_blank' rel='noreferrer'>
             <img className='footer__icon' src={GH} alt="GitHub" />
         </a>
     </footer>
